e7c5b2b28411d2f8866f72312bc4022e238d2848
dutaahmad <dutaahmadtefur@gmail.com>
2024-04-03 01:59:22 +0700

    it worked! need some workaround at handling multiple jpegs

--- src/app/multiple-images-to-pdf/[[...states]]/page.tsx
+++ src/app/multiple-images-to-pdf/[[...states]]/page.tsx
@@ -9,14 +9,17 @@ const MultipleImagesToPDF = async ({
 }: {
     params: { states: string[] };
 }) => {
-
     return (
         <main className="min-h-screen flex flex-col gap-8 items-center justify-center">
-            <h1 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">Multiple Image To PDF Page (Work On Progress)</h1>
+            <div className="flex flex-col items-center">
+                <h1 className="scroll-m-20 text-2xl font-semibold text-center tracking-tight">
+                    Upload maximum 10 (ten) .jpeg images to our platform to
+                    start converting!
+                </h1>
+            </div>
+            <Uploader multiple />
         </main>
     );
 };
 
 export default MultipleImagesToPDF;
-
-
