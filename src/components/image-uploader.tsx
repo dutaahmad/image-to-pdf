142eecf72cf501712984079b39b653d532376aa8
dutaahmad <dutaahmadtefur@gmail.com>
2024-04-03 19:28:49 +0700

    add url search params on Uploader

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,9 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { DialogProps } from "@radix-ui/react-dialog";
 import { CircleCheck, Cross } from "lucide-react";
 
-import Uppy, { type UppyFile, type SuccessResponse } from "@uppy/core";
+import Uppy, {
+    type UppyFile,
+    type SuccessResponse,
+    type UploadResult,
+} from "@uppy/core";
 import { Dashboard } from "@uppy/react";
 import Tus from "@uppy/tus";
 
@@ -27,7 +31,7 @@ import supabase, {
     SUPABASE_SERVICE_KEY,
 } from "@/lib/supabase";
 import { addImageData } from "@/server/server-functions";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { toast } from "sonner";
 
 type UploaderProps = { multiple?: boolean };
@@ -35,6 +39,7 @@ type UploaderProps = { multiple?: boolean };
 function Uploader({ multiple = false }: UploaderProps) {
     const { states } = useParams<{ states: string[] }>();
     const router = useRouter();
+    const searchParams = useSearchParams();
     const [isThereFile, setIsThereFile] = useState<boolean>(false);
     const [uppy] = useState(() =>
         new Uppy({
@@ -62,6 +67,16 @@ function Uploader({ multiple = false }: UploaderProps) {
         })
     );
 
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+            const params = new URLSearchParams(searchParams.toString());
+            params.set(name, value);
+
+            return params.toString();
+        },
+        [searchParams]
+    );
+
     uppy.on("file-added", (file) => {
         file.meta = {
             ...file.meta,
@@ -75,7 +90,7 @@ function Uploader({ multiple = false }: UploaderProps) {
         setIsThereFile(false);
     });
 
-    const onUploadSuccess = async (
+    const onSingleUploadSuccess = async (
         file:
             | UppyFile<Record<string, unknown>, Record<string, unknown>>
             | undefined,
@@ -92,7 +107,6 @@ function Uploader({ multiple = false }: UploaderProps) {
                 name: file!.name,
                 url: data.publicUrl,
             });
-            router.push(`/single-image-to-pdf/${fileObjectName}`);
             toast(
                 <div className="flex gap-8 items-center p-2">
                     <p className="text-base text-right">
@@ -105,6 +119,75 @@ function Uploader({ multiple = false }: UploaderProps) {
                     duration: 3000,
                 }
             );
+            router.push(`/single-image-to-pdf/${fileObjectName}`);
+        } catch (error) {
+            console.error(
+                "Error happened while processing storage data to database. Errors : ",
+                error
+            );
+            toast.error(
+                <div className="flex gap-4">
+                    <p>
+                        Error happened while processing storage data to
+                        database!
+                    </p>
+                    <Cross className="h-4 w-4" />
+                </div>,
+                {
+                    description: new Date().toLocaleDateString(),
+                }
+            );
+        }
+    };
+
+    const onUploadsComplete = async (
+        result: UploadResult<Record<string, unknown>, Record<string, unknown>>
+    ) => {
+        try {
+            const files = result.successful;
+            const filesQueryStringArray: string[] = [];
+            for (const file of files) {
+                // @ts-ignore
+                const fileObjectName: string = file.meta.objectName;
+                const { data } = supabase.storage
+                    .from("images")
+                    .getPublicUrl(fileObjectName);
+                await addImageData({
+                    id: fileObjectName,
+                    name: file!.name,
+                    url: data.publicUrl,
+                });
+                toast(
+                    <div className="flex gap-8 items-center p-2">
+                        <p className="text-base text-right">
+                            Image {fileObjectName} ready to be converted to PDF.
+                        </p>
+                        <CircleCheck className="h-12 w-12" />
+                    </div>,
+                    {
+                        description: <p>{new Date().toLocaleDateString()}</p>,
+                        duration: 3000,
+                    }
+                );
+                const fileQueryString =
+                    createQueryString("file", fileObjectName) + "&";
+                filesQueryStringArray.push(fileQueryString);
+            }
+            const filesQueryString = filesQueryStringArray.join("");
+            toast(
+                <div className="flex gap-8 items-center p-2">
+                    <p className="text-base text-right">
+                        Images ready to be converted to PDF. Redirecting to
+                        convert page...
+                    </p>
+                    <CircleCheck className="h-12 w-12" />
+                </div>,
+                {
+                    description: <p>{new Date().toLocaleDateString()}</p>,
+                    duration: 3000,
+                }
+            );
+            router.push("/multiple-images-to-pdf?" + filesQueryString);
         } catch (error) {
             console.error(
                 "Error happened while processing storage data to database. Errors : ",
@@ -146,12 +229,21 @@ function Uploader({ multiple = false }: UploaderProps) {
     };
 
     useEffect(() => {
-        uppy.on("upload-success", onUploadSuccess);
-        // Tell React to remove the old listener if a different function is passed to the `handleFileUploaded` prop:
-        return () => {
-            uppy.off("upload-success", onUploadSuccess);
-        };
-    }, [onUploadSuccess]);
+        if (multiple) {
+            uppy.on("complete", onUploadsComplete);
+            return () => {
+                uppy.off("complete", onUploadsComplete);
+            };
+        } else {
+            uppy.on("upload-success", onSingleUploadSuccess);
+            // Tell React to remove the old listener if a different function is passed to the `handleFileUploaded` prop:
+            return () => {
+                uppy.off("upload-success", onSingleUploadSuccess);
+            };
+        }
+    }, [onSingleUploadSuccess, onUploadsComplete]);
+
+    console.log("query params : ", searchParams.getAll("file"));
 
     return (
         <Dialog>
