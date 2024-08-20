bd788fe7ad54ae4b05fa21498e0b7f5bc33a4d90
NAHDI DUTA AHMAD <dutaahmadtefur@gmail.com>
2024-08-20 15:18:35 +0700

    tailwind does not work

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
 
 export default defineConfig({
   plugins: [pluginReact(), pluginBabel({
@@ -10,4 +11,9 @@ export default defineConfig({
       opts.plugins?.unshift('babel-plugin-react-compiler');
     },
   }), pluginSvgr()],
+  tools: {
+    rspack: {
+      plugins: [TanStackRouterRspack()]
+    }
+  }
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 
-const App = () => {
+export const App = () => {
   return (
-    <div className="content">
+    <div className='bg-red-900 w-full min-h-screen'>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
